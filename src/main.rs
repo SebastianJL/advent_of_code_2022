d2699276af74562c95e233274df7270e8a604eb4
SebastianJL <johannes.lade@yahoo.com>
2022-12-01 06:20:10 +0100

    Day 01, part 2.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
 use std::error::Error;
 
-use csv::ReaderBuilder;
-
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut reader = ReaderBuilder::new()
-        .delimiter(b' ')
-        .has_headers(false)
-        .from_path("./data/input.txt")?;
-    for result in reader.records() {
-        let record = result?;
-        dbg!(&record.get(1));
-        dbg!(&record);
-        for i in record.iter() {
-            dbg!(i);
+    let mut input = read();
+    input.push('\n');
+    let calories = input
+        .split('\n')
+        .map(|s| s.parse::<u32>().ok());
+
+    let mut totals: Vec<u32> = vec![];
+    let mut sum = 0;
+    for calory in calories {
+        match calory {
+            Some(val) => sum += val,
+            None => {totals.push(sum); sum = 0}
         }
     }
+
+    totals.sort_unstable();
+    let max_3: u32 = totals.iter().rev().take(3).sum();
+    dbg!(max_3);
     Ok(())
 }
 
