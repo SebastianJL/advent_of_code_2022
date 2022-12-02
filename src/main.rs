94c5ff1dd923ac06d24dd263f31e9fecb7fddc58
SebastianJL <johannes.lade@yahoo.com>
2022-12-02 06:23:17 +0100

    Day 02, part 1.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,58 @@
 use std::error::Error;
 
+use itertools::Itertools;
+
 fn main() -> Result<(), Box<dyn Error>> {
     let input = read();
-    let mut totals: Vec<u32> = input
-        .split("\n\n")
-        .map(|elf_string| {
-            elf_string
-                .split('\n')
-                .map(|number| number.parse::<u32>().expect("Couldn't parse integer."))
-                .sum()
+    let mut hands: Vec<(Hand, Hand)> = input
+        .split("\n")
+        .map(|hand_string| -> (Hand, Hand) {
+            hand_string
+                .split(' ')
+                .map(|hand| match hand {
+                    "A" => Hand::Rock,
+                    "B" => Hand::Paper,
+                    "C" => Hand::Scissor,
+                    "X" => Hand::Rock,
+                    "Y" => Hand::Paper,
+                    "Z" => Hand::Scissor,
+                    _ => panic!("Couldn't parse string."),
+                })
+                .collect_tuple().unwrap()
         })
         .collect();
-
-    totals.sort_unstable();
-    let max_3: u32 = totals.iter().rev().take(3).sum();
-    dbg!(max_3);
+    
+    let total: u32 = hands.iter().copied().map(calculate_points).sum();
+    // totals.sort_unstable();
+    // let max_3: u32 = totals.iter().rev().take(3).sum();
+    // dbg!(hands);
+    dbg!(total);
     Ok(())
 }
 
 fn read() -> String {
     std::fs::read_to_string(format!("./data/input.txt")).expect("File not found.")
+}
+
+fn calculate_points((other, me): (Hand, Hand)) -> u32 {
+    use Hand::*;
+    let mut total = match (&me, other) {
+        (Rock, Scissor) | (Scissor, Paper) | (Paper, Rock) => 6,
+        (Rock, Rock) | (Paper, Paper) | (Scissor, Scissor) => 3,
+        _ => 0,
+    };
+
+    total += match me {
+        Rock => 1,
+        Paper => 2,
+        Scissor => 3,
+    };
+    total
+}
+
+#[derive(Debug, Copy, Clone)]
+enum Hand {
+    Rock,
+    Paper,
+    Scissor,
 }
