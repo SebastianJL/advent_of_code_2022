ef1380c42f314cc98f42e45a2bebfb1748050e03
SebastianJL <johannes.lade@yahoo.com>
2022-12-03 07:14:03 +0100

    Day 03, part 2. tuples() instead of batching().

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     let input = read();
     let total: u32 = input
         .lines()
-        .batching(|it| -> Option<u32> {
-            let (elf1, elf2, elf3) = (it.next()?, it.next()?, it.next()?);
+        .tuples()
+        .map(|(elf1, elf2, elf3)| -> u32 {
             type Set = HashSet::<char>;
             let elf1 = Set::from_iter(elf1.chars());
             let elf2 = Set::from_iter(elf2.chars());
             let elf3 = Set::from_iter(elf3.chars());
             let common = elf1.intersection(&elf2).copied().collect::<Set>();
             let common = common.intersection(&elf3).next().expect("Couldn't find common letter.");
-            Some(calculate_value(*common))
+            calculate_value(*common)
         })
         .sum();
     let runtime = start.elapsed();
