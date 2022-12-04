7da20aff971147edcb9244dde26d1a12b75085c1
SebastianJL <johannes.lade@yahoo.com>
2022-12-04 12:45:09 +0100

    Day 04, part 2.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use std::{error::Error, collections::HashSet, time::Instant};
+use std::{error::Error, time::Instant};
 
 use itertools::Itertools;
-
 
 fn main() -> Result<(), Box<dyn Error>> {
     let start = Instant::now();
@@ -10,25 +9,24 @@ fn main() -> Result<(), Box<dyn Error>> {
     let total: u32 = input
         .lines()
         .map(|line| -> u32 {
-            let Some((first, second)) = line.split_once(',') else {
-                panic!("Couldn't split  line.")
+            // Split string.
+            let Some((min1, max1, min2, max2)) = line.split([',', '-']).next_tuple() else {
+                panic!("Couldn't split string");
             };
-            let Some((min1, max1)) = first.split_once('-') else {
-                panic!("Couldn't split first elf.")
-            };
-            let Some((min2, max2)) = second.split_once('-') else {
-                panic!("Couldn't split second elf.")
-            };
+
+            // Parse numbers.
             let (min1, max1): (u32, u32) = (min1.parse().unwrap(), max1.parse().unwrap());
             let (min2, max2): (u32, u32) = (min2.parse().unwrap(), max2.parse().unwrap());
-            if (min1 <= min2 && max2 <= max1) || (min2 <= min1 && max1 <= max2) {
+
+            // Test overlap.
+            if (min1 <= max2) && (min2 <= max1) {
                 1
             } else {
                 0
             }
         })
         .sum();
-    
+
     let runtime = start.elapsed();
     dbg!(total);
     dbg!(runtime);
@@ -37,9 +35,4 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 fn read() -> String {
     std::fs::read_to_string(format!("./data/input.txt")).expect("File not found.")
-}
-
-fn calculate_value(c: char) -> u32 {
-    const CHARS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    CHARS.find(c).unwrap() as u32 + 1
 }
