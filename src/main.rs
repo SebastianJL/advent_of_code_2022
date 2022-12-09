a839a3715c18b3bc946f8dd6db6fe1237b81fac0
SebastianJL <johannes.lade@yahoo.com>
2022-12-09 17:12:34 +0100

    Day 09, part 2.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, str::FromStr, time::Instant, collections::HashSet};
+use std::{collections::HashSet, error::Error, str::FromStr, time::Instant};
 
 use Direction::*;
 
@@ -7,73 +7,62 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let input = read();
     let instructions = parse(&input);
-    let (mut head, mut tail) = (Position { x: 0, y: 0 }, Position { x: 0, y: 0 });
-
+    const N: usize = 10;
+    let mut positions = vec![Position { x: 0, y: 0 }; N];
     let mut visited = HashSet::new();
-    visited.insert(tail);
-    for Instruction{steps, direction} in instructions {
+    visited.insert(positions[N - 1]);
+    for Instruction { steps, direction } in instructions {
         match direction {
             Right => {
                 for _ in 0..steps {
-                    let (dx, dy) = (tail.x - head.x, tail.y - head.y);
-                    match (dx, dy) {
-                        (-1, _) => {
-                            (tail.x, tail.y) = (head.x, head.y);
-                            visited.insert(tail);
-                            head.x += 1
-                        }
-                        (dx, dy) if dx.abs() <= 1 && dy.abs() <= 1 => head.x += 1,
-                        (dx, dy) => panic!("(dx, dy) was {:?}. Should be at most 1.", (dx, dy)),
+                    positions[0].x += 1;
+                    for i in 1..N {
+                        let (head, tail) = (positions[i - 1], positions[i]);
+                        let tail = follow_head(head, tail);
+                        positions[i] = tail;
                     }
-                    // println!("T: {:?}, H: {:?}", tail, head);
+                    visited.insert(positions[N - 1]);
+                    // println!("{positions:?}");
                 }
             }
             Left => {
                 for _ in 0..steps {
-                    let (dx, dy) = (tail.x - head.x, tail.y - head.y);
-                    match (dx, dy) {
-                        (1, _) => {
-                            (tail.x, tail.y) = (head.x, head.y);
-                            visited.insert(tail);
-                            head.x -= 1
-                        }
-                        (dx, dy) if dx.abs() <= 1 && dy.abs() <= 1 => head.x -= 1,
-                        (dx, dy) => panic!("(dx, dy) was {:?}. Should be at most 1.", (dx, dy)),
+                    positions[0].x -= 1;
+                    for i in 1..N {
+                        let (head, tail) = (positions[i - 1], positions[i]);
+                        let tail = follow_head(head, tail);
+                        positions[i] = tail;
                     }
-                    // println!("T: {:?}, H: {:?}", tail, head);
+                    visited.insert(positions[N - 1]);
+                    // println!("{positions:?}");
                 }
-            },
+            }
             Up => {
                 for _ in 0..steps {
-                    let (dx, dy) = (tail.x - head.x, tail.y - head.y);
-                    match (dx, dy) {
-                        (_, -1) => {
-                            (tail.x, tail.y) = (head.x, head.y);
-                            visited.insert(tail);
-                            head.y += 1
-                        }
-                        (dx, dy) if dx.abs() <= 1 && dy.abs() <= 1 => head.y += 1,
-                        (dx, dy) => panic!("(dx, dy) was {:?}. Should be at most 1.", (dx, dy)),
+                    positions[0].y += 1;
+                    for i in 1..N {
+                        let (head, tail) = (positions[i - 1], positions[i]);
+                        let tail = follow_head(head, tail);
+                        positions[i] = tail;
                     }
-                    // println!("T: {:?}, H: {:?}", tail, head);
+                    visited.insert(positions[N - 1]);
+                    // println!("{positions:?}");
                 }
-            },
+            }
             Down => {
                 for _ in 0..steps {
-                    let (dx, dy) = (tail.x - head.x, tail.y - head.y);
-                    match (dx, dy) {
-                        (_, 1) => {
-                            (tail.x, tail.y) = (head.x, head.y);
-                            visited.insert(tail);
-                            head.y -= 1
-                        }
-                        (dx, dy) if dx.abs() <= 1 && dy.abs() <= 1 => head.y -= 1,
-                        (dx, dy) => panic!("(dx, dy) was {:?}. Should be at most 1.", (dx, dy)),
+                    positions[0].y -= 1;
+                    for i in 1..N {
+                        let (head, tail) = (positions[i - 1], positions[i]);
+                        let tail = follow_head(head, tail);
+                        positions[i] = tail;
                     }
-                    // println!("T: {:?}, H: {:?}", tail, head);
+                    visited.insert(positions[N - 1]);
+                    // println!("{positions:?}");
                 }
-            },
+            }
         }
+        // print_pos(&positions);
     }
 
     dbg!(visited.len());
@@ -89,6 +78,61 @@ fn read() -> String {
 
 fn parse(input: &str) -> Vec<Instruction> {
     input.lines().map(|line| line.parse().unwrap()).collect()
+}
+
+fn print_pos(positions: &[Position]) {
+    let xmin: i32 = positions.iter().map(|pos| pos.x).min().unwrap();
+    let xmax: i32 = positions.iter().map(|pos| pos.x).max().unwrap();
+    let ymin: i32 = positions.iter().map(|pos| pos.y).min().unwrap();
+    let ymax: i32 = positions.iter().map(|pos| pos.y).max().unwrap();
+
+    for row in (xmin..=xmax).rev() {
+        for col in ymin..=ymax {
+            if let Some(index) = positions
+                .iter()
+                .position(|&p| p == Position { x: row, y: col })
+            {
+                print!("{}", index);
+            } else {
+                print!(".");
+            }
+            print!("\n");
+        }
+    }
+    print!("\n");
+}
+
+fn follow_head(head: Position, mut tail: Position) -> Position {
+    let (dx, dy) = (head.x - tail.x, head.y - tail.y);
+
+    match (dx, dy) {
+        // Outer ring.
+        (1, 2) | (2, 2) | (2, 1) => {
+            tail.x += 1;
+            tail.y += 1
+        }
+        (2, 0) => tail.x += 1,
+        (2, -1) | (2, -2) | (1, -2) => {
+            tail.x += 1;
+            tail.y -= 1
+        }
+        (0, -2) => tail.y -= 1,
+        (-1, -2) | (-2, -2) | (-2, -1) => {
+            tail.x -= 1;
+            tail.y -= 1
+        }
+        (-2, 0) => tail.x -= 1,
+        (-2, 1) | (-2, 2) | (-1, 2) => {
+            tail.x -= 1;
+            tail.y += 1
+        }
+        (0, 2) => tail.y += 1,
+        // Inner square.
+        (dx, dy) if dx.abs() <= 1 && dy.abs() <= 1 => {}
+        // Outside of 5x5 nearest neighbors.
+        (dx, dy) => panic!("(dx, dy) was {:?}. Should be at most 2.", (dx, dy)),
+    };
+    tail
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
