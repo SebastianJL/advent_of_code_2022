a8ebe79d21a692e1471bab6e6a5bb4bad05ca5ee
SebastianJL <johannes.lade@yahoo.com>
2022-12-11 14:55:06 +0100

    Day 11, part 1.

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,93 @@
-use std::{collections::HashSet, error::Error, str::FromStr, time::Instant};
-
-use Direction::*;
+use std::{collections::VecDeque, error::Error, str::FromStr, time::Instant};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let start = Instant::now();
 
-    let input = read();
-    let instructions = parse(&input);
-    const N: usize = 10;
-    let mut positions = vec![Position { x: 0, y: 0 }; N];
-    let mut visited = HashSet::new();
-    visited.insert(positions[N - 1]);
-    for Instruction { steps, direction } in instructions {
-        for _ in 0..steps {
-            // Move head.
-            match direction {
-                Right => positions[0].x += 1,
-                Left => positions[0].x -= 1,
-                Up => positions[0].y += 1,
-                Down => positions[0].y -= 1,
-            };
-            // Follow with tail.
-            for i in 1..N {
-                let (head, tail) = (positions[i - 1], positions[i]);
-                let tail = follow_head(head, tail);
-                positions[i] = tail;
+    // let mut monkeys = vec![
+    //     Monkey {
+    //         items: vec![79, 98].into(),
+    //         op: |old| old * 19,
+    //         divisible: |val| if val % 23 == 0 { 2 } else { 3 },
+    //     },
+    //     Monkey {
+    //         items: vec![54, 65, 75, 74].into(),
+    //         op: |old| old + 6,
+    //         divisible: |val| if val % 19 == 0 { 2 } else { 0 },
+    //     },
+    //     Monkey {
+    //         items: vec![79, 60, 97].into(),
+    //         op: |old| old * old,
+    //         divisible: |val| if val % 13 == 0 { 1 } else { 3 },
+    //     },
+    //     Monkey {
+    //         items: vec![74].into(),
+    //         op: |old| old + 3,
+    //         divisible: |val| if val % 17 == 0 { 0 } else { 1 },
+    //     },
+    // ];
+
+    let mut monkeys = vec![
+        Monkey {
+            items: vec![89, 73, 66, 57, 64, 80].into(),
+            op: |old| old * 3,
+            divisible: |val| if val % 13 == 0 { 6 } else { 2 },
+        },
+        Monkey {
+            items: vec![83, 78, 81, 55, 81, 59, 69].into(),
+            op: |old| old + 1,
+            divisible: |val| if val % 3 == 0 { 7 } else { 4 },
+        },
+        Monkey {
+            items: vec![76, 91, 58, 85].into(),
+            op: |old| old * 13,
+            divisible: |val| if val % 7 == 0 { 1 } else { 4 },
+        },
+        Monkey {
+            items: vec![71, 72, 74, 76, 68].into(),
+            op: |old| old * old,
+            divisible: |val| if val % 2 == 0 { 6 } else { 0 },
+        },
+        Monkey {
+            items: vec![98, 85, 84].into(),
+            op: |old| old + 7,
+            divisible: |val| if val % 19 == 0 { 5 } else { 7 },
+        },
+        Monkey {
+            items: vec![78].into(),
+            op: |old| old + 8,
+            divisible: |val| if val % 5 == 0 { 3 } else { 0 },
+        },
+        Monkey {
+            items: vec![86, 70, 60, 88, 88, 78, 74, 83].into(),
+            op: |old| old + 4,
+            divisible: |val| if val % 11 == 0 { 1 } else { 2 },
+        },
+        Monkey {
+            items: vec![81, 58].into(),
+            op: |old| old + 5,
+            divisible: |val| if val % 17 == 0 { 3 } else { 5 },
+        },
+    ];
+
+    let n_rounds = 20;
+    let mut inspected = vec![0; monkeys.len()];
+    for _ in 0..n_rounds {
+        for i in 0..monkeys.len() {
+            let monkey = &mut monkeys[i];
+            let n_inspected = monkey.inspect();
+            inspected[i] += n_inspected;
+            monkey.apply(|item| item / 3);
+            let throw_instructions = monkey.throw();
+            for (dest, item) in throw_instructions {
+                monkeys[dest].items.push_back(item);
             }
-            // Insert tail.
-            visited.insert(positions[N - 1]);
         }
-        print_pos(&positions);
     }
 
-    print_visited(&visited);
-    dbg!(visited.len());
+    println!("{monkeys:?}");
+    inspected.sort_by(|a, b| b.cmp(a));
+    let total = inspected[0] * inspected[1];
+    dbg!(total);
 
     let runtime = start.elapsed();
     dbg!(runtime);
@@ -44,119 +98,33 @@ fn read() -> String {
     std::fs::read_to_string("input.txt").expect("File not found.")
 }
 
-fn parse(input: &str) -> Vec<Instruction> {
-    input.lines().map(|line| line.parse().unwrap()).collect()
-}
-
-fn print_pos(positions: &[Position]) {
-    let xmin: i32 = positions.iter().map(|pos| pos.x).min().unwrap().min(0);
-    let xmax: i32 = positions.iter().map(|pos| pos.x).max().unwrap().max(0);
-    let ymin: i32 = positions.iter().map(|pos| pos.y).min().unwrap().min(0);
-    let ymax: i32 = positions.iter().map(|pos| pos.y).max().unwrap().max(0);
-    for row in (ymin..=ymax).rev() {
-        for col in xmin..=xmax {
-            if let Some(index) = positions
-                .iter()
-                .position(|&p| p == Position { x: col, y: row })
-            {
-                print!("{}", index);
-            } else if (row, col) == (0, 0) {
-                print!("s");
-            } else {
-                print!(".");
-            }
-        }
-        print!("\n");
-    }
-    print!("\n");
-}
-
-fn print_visited(visited: &HashSet<Position>) {
-    let xmin: i32 = visited.iter().map(|pos| pos.x).min().unwrap().min(0);
-    let xmax: i32 = visited.iter().map(|pos| pos.x).max().unwrap().max(0);
-    let ymin: i32 = visited.iter().map(|pos| pos.y).min().unwrap().min(0);
-    let ymax: i32 = visited.iter().map(|pos| pos.y).max().unwrap().max(0);
-    for row in (ymin..=ymax).rev() {
-        for col in xmin..=xmax {
-            if visited.contains(&Position { x: col, y: row }) {
-                print!("#");
-            } else if (row, col) == (0, 0) {
-                print!("s");
-            } else {
-                print!(".");
-            }
-        }
-        print!("\n");
-    }
-    print!("\n");
-}
-
-fn follow_head(head: Position, mut tail: Position) -> Position {
-    let (dx, dy) = (head.x - tail.x, head.y - tail.y);
-
-    match (dx, dy) {
-        // Outer ring.
-        (1, 2) | (2, 2) | (2, 1) => {
-            tail.x += 1;
-            tail.y += 1
-        }
-        (2, 0) => tail.x += 1,
-        (2, -1) | (2, -2) | (1, -2) => {
-            tail.x += 1;
-            tail.y -= 1
-        }
-        (0, -2) => tail.y -= 1,
-        (-1, -2) | (-2, -2) | (-2, -1) => {
-            tail.x -= 1;
-            tail.y -= 1
-        }
-        (-2, 0) => tail.x -= 1,
-        (-2, 1) | (-2, 2) | (-1, 2) => {
-            tail.x -= 1;
-            tail.y += 1
-        }
-        (0, 2) => tail.y += 1,
-        // Inner square.
-        (dx, dy) if dx.abs() <= 1 && dy.abs() <= 1 => {}
-        // Outside of 5x5 nearest neighbors.
-        (dx, dy) => panic!("(dx, dy) was {:?}. Should be at most 2.", (dx, dy)),
-    };
-    tail
-}
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
-struct Position {
-    x: i32,
-    y: i32,
-}
-
 #[derive(Debug)]
-enum Direction {
-    Left,
-    Right,
-    Up,
-    Down,
+struct Monkey {
+    items: VecDeque<u32>,
+    op: fn(u32) -> u32,
+    divisible: fn(u32) -> usize,
 }
 
-#[derive(Debug)]
-struct Instruction {
-    steps: usize,
-    direction: Direction,
-}
+impl Monkey {
+    fn inspect(&mut self) -> usize {
+        let n_inspected = self.items.len();
+        for item in self.items.iter_mut() {
+            *item = (self.op)(*item);
+        }
+        n_inspected
+    }
 
-impl FromStr for Instruction {
-    type Err = String;
+    fn apply(&mut self, f: fn(u32) -> u32) {
+        for item in self.items.iter_mut() {
+            *item = (f)(*item);
+        }
+    }
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (direction, steps) = s.split_once(' ').ok_or("Couldn't split.")?;
-        let steps = steps.parse().or(Err("Couldn't parse distance."))?;
-        let direction = match direction {
-            "L" => Left,
-            "R" => Right,
-            "U" => Up,
-            "D" => Down,
-            _ => Err("Invalid direction.")?,
-        };
-        Ok(Instruction { steps, direction })
+    fn throw(&mut self) -> Vec<(usize, u32)> {
+        let items = std::mem::replace(&mut self.items, vec![].into());
+        items
+            .into_iter()
+            .map(|item| ((self.divisible)(item), item))
+            .collect()
     }
 }
