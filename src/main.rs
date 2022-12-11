08da8f72cf1a56d98c221443f0abe67e39731563
SebastianJL <johannes.lade@yahoo.com>
2022-12-11 15:20:25 +0100

    Day 11, part 2.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::VecDeque, error::Error, str::FromStr, time::Instant};
+use std::{collections::VecDeque, error::Error, str::FromStr, time::Instant, thread};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let start = Instant::now();
@@ -7,22 +7,26 @@ fn main() -> Result<(), Box<dyn Error>> {
     //     Monkey {
     //         items: vec![79, 98].into(),
     //         op: |old| old * 19,
-    //         divisible: |val| if val % 23 == 0 { 2 } else { 3 },
+    //         division: (23, 2, 3),
+    //         // divisible: |val| if val % 23 == 0 { 2 } else { 3 },
     //     },
     //     Monkey {
     //         items: vec![54, 65, 75, 74].into(),
     //         op: |old| old + 6,
-    //         divisible: |val| if val % 19 == 0 { 2 } else { 0 },
+    //         division: (19, 2, 0),
+    //         // divisible: |val| if val % 19 == 0 { 2 } else { 0 },
     //     },
     //     Monkey {
     //         items: vec![79, 60, 97].into(),
     //         op: |old| old * old,
-    //         divisible: |val| if val % 13 == 0 { 1 } else { 3 },
+    //         division: (13, 1, 3),
+    //         // divisible: |val| if val % 13 == 0 { 1 } else { 3 },
     //     },
     //     Monkey {
     //         items: vec![74].into(),
     //         op: |old| old + 3,
-    //         divisible: |val| if val % 17 == 0 { 0 } else { 1 },
+    //         division: (17, 0, 1),
+    //         // divisible: |val| if val % 17 == 0 { 0 } else { 1 },
     //     },
     // ];
 
@@ -30,53 +34,59 @@ fn main() -> Result<(), Box<dyn Error>> {
         Monkey {
             items: vec![89, 73, 66, 57, 64, 80].into(),
             op: |old| old * 3,
-            divisible: |val| if val % 13 == 0 { 6 } else { 2 },
+            division: (13, 6, 2),
         },
         Monkey {
             items: vec![83, 78, 81, 55, 81, 59, 69].into(),
             op: |old| old + 1,
-            divisible: |val| if val % 3 == 0 { 7 } else { 4 },
+            division: (3, 7, 4),
         },
         Monkey {
             items: vec![76, 91, 58, 85].into(),
             op: |old| old * 13,
-            divisible: |val| if val % 7 == 0 { 1 } else { 4 },
+            division: (7, 1, 4),
         },
         Monkey {
             items: vec![71, 72, 74, 76, 68].into(),
             op: |old| old * old,
-            divisible: |val| if val % 2 == 0 { 6 } else { 0 },
+            division: (2, 6, 0),
         },
         Monkey {
             items: vec![98, 85, 84].into(),
             op: |old| old + 7,
-            divisible: |val| if val % 19 == 0 { 5 } else { 7 },
+            division: (19, 5, 7),
         },
         Monkey {
             items: vec![78].into(),
             op: |old| old + 8,
-            divisible: |val| if val % 5 == 0 { 3 } else { 0 },
+            division: (5, 3, 0),
         },
         Monkey {
             items: vec![86, 70, 60, 88, 88, 78, 74, 83].into(),
             op: |old| old + 4,
-            divisible: |val| if val % 11 == 0 { 1 } else { 2 },
+            division: (11, 1, 2),
         },
         Monkey {
             items: vec![81, 58].into(),
             op: |old| old + 5,
-            divisible: |val| if val % 17 == 0 { 3 } else { 5 },
+            division: (17, 3, 5),
         },
     ];
 
-    let n_rounds = 20;
+    let n_rounds = 10_000;
     let mut inspected = vec![0; monkeys.len()];
+    let lcm = monkeys
+        .iter()
+        .map(|m| m.division.0)
+        .reduce(num::integer::lcm)
+        .unwrap();
+    dbg!(lcm*lcm);
     for _ in 0..n_rounds {
         for i in 0..monkeys.len() {
             let monkey = &mut monkeys[i];
             let n_inspected = monkey.inspect();
             inspected[i] += n_inspected;
-            monkey.apply(|item| item / 3);
+            monkey.apply(|item| item % lcm);
             let throw_instructions = monkey.throw();
             for (dest, item) in throw_instructions {
                 monkeys[dest].items.push_back(item);
@@ -100,9 +110,9 @@ fn read() -> String {
 
 #[derive(Debug)]
 struct Monkey {
-    items: VecDeque<u32>,
-    op: fn(u32) -> u32,
-    divisible: fn(u32) -> usize,
+    items: VecDeque<u64>,
+    op: fn(u64) -> u64,
+    division: (u64, usize, usize),
 }
 
 impl Monkey {
@@ -114,17 +124,25 @@ impl Monkey {
         n_inspected
     }
 
-    fn apply(&mut self, f: fn(u32) -> u32) {
+    fn apply(&mut self, f: impl Fn(u64) -> u64) {
         for item in self.items.iter_mut() {
             *item = (f)(*item);
         }
     }
 
-    fn throw(&mut self) -> Vec<(usize, u32)> {
+    fn divisible(&self, item: u64) -> usize {
+        if item % self.division.0 == 0 {
+            self.division.1
+        } else {
+            self.division.2
+        }
+    }
+
+    fn throw(&mut self) -> Vec<(usize, u64)> {
         let items = std::mem::replace(&mut self.items, vec![].into());
         items
             .into_iter()
-            .map(|item| ((self.divisible)(item), item))
+            .map(|item| (self.divisible(item), item))
             .collect()
     }
 }
