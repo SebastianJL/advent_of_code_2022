4a8dd073032d2e09c05b6f10874b791b570b1443
SebastianJL <johannes.lade@yahoo.com>
2022-12-13 17:59:34 +0100

    Day 13, part 1. (unfinished 2)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![feature(iter_advance_by)]
-use std::{error::Error, str::FromStr, time::Instant};
+use std::{error::Error, str::FromStr, time::Instant, vec};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let start = Instant::now();
@@ -7,10 +7,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     let input = read();
     let lists = parse(&input)?;
 
-    for (list1, list2) in lists.iter().copied() {
-        let (list1, list2): (List, List) = (list1.parse()?, list2.parse()?);
-        println!("{:}\n{:}\n", list1, list2);
+    let mut sum = 0;
+    for (i, (list1, list2)) in lists.iter().copied().enumerate() {
+        let (list1, list2): (List<u32>, List<u32>) = (list1.parse()?, list2.parse()?);
+        println!("{:}\n{:}", list1, list2);
+        if list1 < list2 {
+            sum += i + 1;
+        }
     }
+    dbg!(sum);
 
     let runtime = start.elapsed();
     dbg!(runtime);
@@ -33,28 +38,28 @@ fn parse(input: &str) -> Result<Vec<(&str, &str)>, String> {
         .collect::<Result<_, _>>()
 }
 
-#[derive(Debug)]
-enum Item {
-    Val(u32),
-    List(List),
+#[derive(Debug, Clone, PartialEq)]
+enum Item<T> {
+    Val(T),
+    List(List<T>),
 }
 
-#[derive(Debug)]
-struct List {
-    items: Vec<Item>,
+#[derive(Debug, Clone, PartialEq)]
+struct List<T> {
+    items: Vec<Item<T>>,
 }
 
-impl List {
-    fn new(items: Vec<Item>) -> Self {
-        List { items }
+impl<T> List<T> {
+    fn new() -> Self {
+        List { items: vec![] }
     }
 
-    fn push(&mut self, item: Item) {
+    fn push(&mut self, item: Item<T>) {
         self.items.push(item);
     }
 }
 
-impl FromStr for List {
+impl FromStr for List<u32> {
     type Err = String;
 
     fn from_str(mut s: &str) -> Result<Self, Self::Err> {
@@ -63,7 +68,7 @@ impl FromStr for List {
             s = &s[1..s.len() - 1];
         }
         // dbg!(s);
-        let mut list: List = List::new(vec![]);
+        let mut list: List<u32> = List::new();
         let mut iter = s.chars().enumerate();
         while let Some((i0, c)) = iter.next() {
             match c {
@@ -73,7 +78,7 @@ impl FromStr for List {
                     let i1 = i0 + di;
                     // println!("{i0}, {i1}");
                     // println!("{:?}", &s[i0..=i1]);
-                    let sublist: List = s[i0..=i1].parse()?;
+                    let sublist: List<u32> = s[i0..=i1].parse()?;
                     // println!("{:?}", sublist);
                     list.push(Item::List(sublist));
                     iter.advance_by(i1 - i0)
@@ -91,9 +96,9 @@ impl FromStr for List {
     }
 }
 
-impl std::fmt::Display for List {
+impl<T: std::fmt::Display> std::fmt::Display for List<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "[");
+        write!(f, "[")?;
         let len = self.items.len();
         if len > 0 {
             for item in self.items.iter().take(len - 1) {
@@ -123,6 +128,31 @@ impl std::fmt::Display for List {
     }
 }
 
+impl<T: PartialOrd + Clone> PartialOrd for Item<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        match (self, other) {
+            (Item::Val(s), Item::Val(o)) => s.partial_cmp(o),
+            (s @ Item::Val(_), Item::List(o)) => {
+                let mut l = List::new();
+                l.push(s.clone());
+                l.partial_cmp(o)
+            }
+            (Item::List(s), o @ Item::Val(_)) => {
+                let mut l = List::new();
+                l.push(o.clone());
+                s.partial_cmp(&l)
+            },
+            (Item::List(s), Item::List(o)) => s.partial_cmp(o),
+        }
+    }
+}
+
+impl<T: PartialOrd + Clone> PartialOrd for List<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.items.partial_cmp(&other.items)
+    }
+}
+
 fn find_matching_paren(s: &str) -> usize {
     assert!(s.starts_with('['));
     let mut stack = 0;
@@ -142,4 +172,3 @@ fn find_matching_paren(s: &str) -> usize {
     }
     index
 }
-
