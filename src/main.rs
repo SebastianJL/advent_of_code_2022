16f5501b2bd2493678134d6c045ad320d0aaa4c4
SebastianJL <johannes.lade@yahoo.com>
2022-12-18 16:10:55 +0100

    Day 16, part 1. (working??)
    It works for the input but not for the example.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
-use std::{
-    collections::{HashMap},
-    error::Error,
-    time::Instant,
-};
+use std::{collections::HashMap, error::Error, time::Instant};
 
 use regex::Regex;
 
-type Graph<'a> = HashMap<&'a str, (u32, Vec<&'a str>)>;
+#[derive(Debug)]
+struct Node {
+    flow: u32,
+    next: Vec<usize>,
+}
+type Graph<'a> = Vec<Node>;
+type State = (usize, u64, u32);
 
 fn main() -> Result<(), Box<dyn Error>> {
     let start = Instant::now();
 
     let input = read();
-    let graph = parse(&input);
+    let (start_node, graph) = parse(&input);
 
-    let start_node = "AA";
-    let mut visited = Vec::new();
+    let opened = 0_u64;
     let depth = 30;
-    let res = max_flow(start_node, &graph, &mut visited, depth);
+    let mut cache = HashMap::new();
+    // println!("{graph:?}");
+    let res = max_flow(start_node, &graph, opened, depth, &mut cache);
     dbg!(res);
 
     let runtime = start.elapsed();
@@ -29,45 +32,54 @@ fn read() -> String {
     std::fs::read_to_string("input.txt").expect("File not found.")
 }
 
-fn max_flow<'a>(node: &'a str, g: &'a Graph, visited: &mut Vec<&'a str>, depth: u32) -> u32 {
-    let my_node = node;
-    let (my_flow, next_nodes) = g.get(my_node).unwrap();
+fn max_flow<'a>(
+    start_node: usize,
+    graph: &'a Graph,
+    mut open: u64,
+    depth: u32,
+    cache: &mut HashMap<State, u32>,
+) -> u32 {
+    // If result has been cached return it directly.
+    let state = (start_node, open, depth);
+    if let Some(res) = cache.get(&state) {
+        return *res;
+    }
+    if depth < 2 {
+        return 0;
+    }
 
-    if depth == 2 {
-        return my_flow * (depth - 1);
-    } else if depth < 2 {
-        return 0;
-    }
-    // dbg!(visited.len());
-    if g.len() == visited.len() {
-        return 0;
-    }
+    let my_node = start_node;
+    let Node {
+        flow: my_flow,
+        next: next_nodes,
+    } = graph.get(my_node).unwrap();
+
 
     // Open your own valve then move on.
     let mut option_1 = 0;
-    if !visited.contains(&my_node) && depth >= 2 && *my_flow != 0 {
-        visited.push(my_node);
-        // dbg!(&visited);
+    if *my_flow != 0 && (open & (1 << my_node) == 0) {
+        open |= 1 << my_node;
         option_1 = my_flow * (depth - 1)
             + next_nodes
                 .iter()
-                .map(|node| max_flow(node, g, visited, depth - 2))
+                .map(|node| max_flow(*node, graph, open, depth - 2, cache))
                 .max()
                 .unwrap();
-        visited.pop();
     }
 
     // Don't open your own valve. Move on directly.
     let option_2 = next_nodes
         .iter()
-        .map(|node| max_flow(node, g, visited, depth - 1))
+        .map(|node| max_flow(*node, graph, open, depth - 1, cache))
         .max()
         .unwrap();
 
-    return option_1.max(option_2);
+    let res = option_1.max(option_2);
+    cache.insert(state, res);
+    res
 }
 
-fn parse(input: &str) -> Graph {
+fn parse(input: &str) -> (usize, Graph) {
     let re_nodes = Regex::new(r"[A-Z][A-Z]").unwrap();
     let re_flow = Regex::new(r"\d+").unwrap();
     let mut hm = HashMap::new();
@@ -83,5 +95,24 @@ fn parse(input: &str) -> Graph {
         hm.insert(key, (flow, nodes));
     }
 
-    hm
+    let mut what: Vec<_> = hm.into_iter().collect();
+    // Sort what by flow.
+    what.sort_by(|(_, (flow_a, _)), (_, (flow_b, _))| flow_b.cmp(flow_a));
+    let graph: Vec<_> = what
+        .iter()
+        .cloned()
+        .map(|(_, (flow, nodes))| Node {
+            flow,
+            next: nodes
+                .into_iter()
+                .map(|node_a| {
+                    what.iter()
+                        .position(|(node_b, _)| *node_b == node_a)
+                        .unwrap()
+                })
+                .collect(),
+        })
+        .collect();
+    let start_node = what.iter().position(|(node, _)| *node == "AA").unwrap();
+    (start_node, graph)
 }
