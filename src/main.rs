08792612bc71d1ffab31476821fce18e7eca040f
SebastianJL <johannes.lade@yahoo.com>
2022-12-26 15:42:02 +0100

    Day 10, part 1.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
-use std::{cell::RefCell, error::Error, rc::Rc, time::Instant};
+use std::{error::Error, time::Instant};
+
+use itertools::Itertools;
+use nom::{
+    branch::alt, bytes::complete::tag, character::complete::line_ending, multi::separated_list1,
+    sequence::separated_pair, IResult,
+};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let start = Instant::now();
 
     let input = read();
-    let mut fs = parse_input(&input);
-    fs.print();
+    let instructions = parse_instructions(&input);
+    // dbg!(&instructions);
 
-    let total_space = 70_000_000;
-    let update_size = 30_000_000;
-
-    let available_space = total_space - fs.disk_usage();
-    let need_to_free = update_size - available_space;
-
-    let dir_sizes = fs.gather_dir_sizes();
-    let small_dirs = dir_sizes.iter().filter(|(_, size)| *size >= need_to_free);
-    let smallest_possible_dir = small_dirs.map(|(_, size)| size).min().unwrap();
-    dbg!(smallest_possible_dir);
+    let mut cpu = Cpu::from(instructions);
+    cpu.x = 1;
+    let total = cpu.run();
+    dbg!(total);
 
     let runtime = start.elapsed();
     dbg!(runtime);
@@ -27,132 +27,92 @@ fn read() -> String {
     std::fs::read_to_string("input.txt").expect("File not found.")
 }
 
-fn parse_input(input: &str) -> FileSystem {
-    let root = Rc::new(RefCell::new(Directory::new("/")));
-    let mut current_dir = Rc::clone(&root);
-    let mut iter = input.lines().peekable();
-    iter.next().unwrap();
-    while let Some(line) = iter.next() {
-        if line.starts_with("$ cd ..") {
-            let parent = Rc::clone(current_dir.borrow().parent.as_ref().unwrap());
-            current_dir = parent;
-        } else if let Some(name) = line.strip_prefix("$ cd ") {
-            let mut new_dir = Directory::new(name);
-            new_dir.parent = Some(Rc::clone(&current_dir));
-            let new_dir = Rc::new(RefCell::new(new_dir));
-            current_dir.borrow_mut().dirs.push(Rc::clone(&new_dir));
-            current_dir = new_dir;
-        } else if line.starts_with("$ ls") {
-            while let Some(next_line) = iter.peek() {
-                if next_line.starts_with('$') {
-                    break;
-                } else if next_line.starts_with("dir") {
-                    iter.next().unwrap();
-                } else {
-                    let line = iter.next().unwrap();
-                    let (size, name) = line.split_once(' ').unwrap();
-                    let size: u32 = size.parse().unwrap();
-                    current_dir.borrow_mut().files.push(File::new(name, size));
-                }
+fn parse_instructions(input: &str) -> Vec<Instruction> {
+    let (_, instructions) = separated_list1(line_ending, parse_instruction)(input).unwrap();
+    instructions
+}
+
+fn parse_instruction(input: &str) -> IResult<&str, Instruction> {
+    let (input, instruction) = alt((parse_addx, parse_noop))(input)?;
+    Ok((input, instruction))
+}
+
+fn parse_addx(input: &str) -> IResult<&str, Instruction> {
+    let (input, (_, num)) =
+        separated_pair(tag("addx"), tag(" "), nom::character::complete::i32)(input)?;
+    Ok((input, Instruction::Addx(num)))
+}
+
+fn parse_noop(input: &str) -> IResult<&str, Instruction> {
+    let (input, _) = tag("noop")(input)?;
+    Ok((input, Instruction::Noop))
+}
+
+#[derive(Debug, Copy, Clone)]
+enum Instruction {
+    Noop,
+    Addx(i32),
+}
+
+struct Cpu {
+    state: CpuState,
+    instructions: Vec<Instruction>,
+    x: i32,
+    counter: u32,
+}
+
+#[derive(PartialEq, Eq, Debug)]
+enum CpuState {
+    ReadInstruction,
+    Addx(i32),
+    Finished,
+}
+
+impl From<Vec<Instruction>> for Cpu {
+    fn from(instructions: Vec<Instruction>) -> Self {
+        Cpu {
+            state: CpuState::ReadInstruction,
+            instructions,
+            x: 0,
+            counter: 0,
+        }
+    }
+}
+
+impl Cpu {
+    fn run(&mut self) -> i32 {
+        use CpuState::*;
+
+        self.counter = 0;
+        let mut iter = self.instructions.iter();
+
+        let cycles: Vec<_> = (20..=220).step_by(40).collect();
+        let mut total = 0;
+
+        while self.state != Finished {
+            self.counter += 1;
+
+            if cycles.contains(&self.counter) {
+                total += self.counter as i32 * self.x;
+                println!("{} {} {:?}", self.counter, self.x, self.state);
             }
+
+            let new_state = match self.state {
+                ReadInstruction => match iter.next() {
+                    None => Finished,
+                    Some(Instruction::Noop) => ReadInstruction,
+                    Some(Instruction::Addx(num)) => Addx(*num),
+                },
+                Addx(num) => {
+                    self.x += num;
+                    ReadInstruction
+                }
+                Finished => Finished,
+            };
+
+            self.state = new_state;
         }
-    }
 
-    FileSystem {
-        root: Rc::clone(&root),
-    }
-}
-
-#[derive(Debug)]
-struct File {
-    name: String,
-    size: u32,
-}
-
-impl File {
-    fn new(name: &str, size: u32) -> Self {
-        File {
-            name: name.to_owned(),
-            size,
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Directory {
-    name: String,
-    parent: Option<Rc<RefCell<Directory>>>,
-    dirs: Vec<Rc<RefCell<Directory>>>,
-    files: Vec<File>,
-    size: Option<u32>,
-}
-
-impl Directory {
-    fn new(name: &str) -> Self {
-        Directory {
-            name: name.to_owned(),
-            parent: None,
-            dirs: vec![],
-            files: vec![],
-            size: None,
-        }
-    }
-
-    fn print(&self, depth: usize) {
-        println!("{:>pad$}- {:} (dir)", "", self.name, pad = 2 * depth);
-        for dir in &self.dirs {
-            dir.borrow().print(depth + 1);
-        }
-        for file in &self.files {
-            println!(
-                "{:>pad$}- {:} (file, size={})",
-                "",
-                file.name,
-                file.size,
-                pad = 2 * (depth + 1),
-            );
-        }
-    }
-
-    fn size(&mut self) -> u32 {
-        if let Some(size) = self.size {
-            return size;
-        }
-        let mut size = self.files.iter().map(|file| file.size).sum();
-        size += self
-            .dirs
-            .iter()
-            .map(|dir| dir.borrow_mut().size())
-            .sum::<u32>();
-        self.size = Some(size);
-        size
-    }
-
-    fn append_dir(&mut self, dirs: &mut Vec<(String, u32)>) {
-        dirs.push((self.name.clone(), self.size()));
-        for dir in self.dirs.iter_mut() {
-            dir.borrow_mut().append_dir(dirs);
-        }
-    }
-}
-
-struct FileSystem {
-    root: Rc<RefCell<Directory>>,
-}
-
-impl FileSystem {
-    fn print(&self) {
-        self.root.borrow().print(0);
-    }
-
-    fn disk_usage(&mut self) -> u32 {
-        self.root.borrow_mut().size()
-    }
-
-    fn gather_dir_sizes(&mut self) -> Vec<(String, u32)> {
-        let mut dirs = vec![];
-        self.root.borrow_mut().append_dir(&mut dirs);
-
-        dirs
+        total
     }
 }
